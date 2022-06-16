59a658c9e87ac1395687afe5d1bcc79bbe26cdac
Yukimasa Kuwabara <yukkin1204.mathematics@gmail.com>
2022-06-17 01:48:12 +0900

    ABC253 D

---
+++ ABC253/253_D.py
@@ -0,0 +1,20 @@
+# FizzBuzz Sum Hard
+import math
+
+N, A, B = map(int, input().split())
+
+q_A = N // A
+A_end = A * q_A
+q_B = N // B
+B_end = B * q_B
+
+C = A * B // math.gcd(A, B)
+q_C = N // C
+C_end = C * q_C
+
+sum_N = N * (N + 1) // 2
+sum_A = q_A * (A + A_end) // 2
+sum_B = q_B * (B + B_end) // 2
+sum_C = q_C * (C + C_end) // 2
+
+print(sum_N - (sum_A + sum_B - sum_C))
